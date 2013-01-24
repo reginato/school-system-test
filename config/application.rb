9e728665d39b67780ebabd06fe969cd907ad7549
Rodrigo Reginato Marques <rreginatom@gmail.com>
2013-01-24 14:05:29 -0200

    Adding generators.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ end
 
 module SchoolSystemTest
   class Application < Rails::Application
+    # Do not generate specs for views and requests. Also, do not generate assets.
+    config.generators do |g|
+      g.helper false
+      g.view_specs false
+      g.assets false
+      g.integration_tool false
+    end
+
+    # Prevent initializing your application and connect to the database on assets precompile.
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
