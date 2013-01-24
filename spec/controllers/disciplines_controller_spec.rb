99d8a80943f129912c286d6bf1c3c08d38ffe9aa
Rodrigo Reginato Marques <rreginatom@gmail.com>
2013-01-24 16:50:14 -0200

    Improve controller discipline test

--- spec/controllers/disciplines_controller_spec.rb
+++ spec/controllers/disciplines_controller_spec.rb
@@ -16,131 +16,117 @@ describe DisciplinesController do
     {}
   end
 
-  describe "GET index" do
-    it "assigns all disciplines as @disciplines" do
-      discipline = Discipline.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:disciplines).should eq([discipline])
+  describe "GET 'index'" do
+    let!(:discipline) { FactoryGirl.create(:discipline) }
+    
+    before do
+      get 'index'
     end
+    
+    it { response.should be_success }
+    it { assigns(:disciplines).should == [discipline] }
   end
 
   describe "GET show" do
+    let!(:discipline) { FactoryGirl.create(:discipline) }
+    
     it "assigns the requested discipline as @discipline" do
-      discipline = Discipline.create! valid_attributes
-      get :show, {:id => discipline.to_param}, valid_session
+      get :show, :id => discipline.id
       assigns(:discipline).should eq(discipline)
     end
   end
 
-  describe "GET new" do
-    it "assigns a new discipline as @discipline" do
-      get :new, {}, valid_session
-      assigns(:discipline).should be_a_new(Discipline)
+  describe "GET 'new'" do
+    before do
+      get 'new'
+    end
+
+    it { response.should be_success }
+    it { assigns(:discipline).should be_instance_of Discipline }
+    it { render_template 'new' }
+  end
+
+  describe "GET 'edit" do
+    let!(:discipline) { FactoryGirl.create(:discipline) }
+
+    before do
+      get 'edit', :id => discipline.to_param
+    end
+  
+    it { response.should be_success }
+    it { assigns(:discipline).should be_instance_of Discipline }
+    it { render_template 'edit' }
+  end
+
+  describe "POST create" do          
+    context "with valid params" do
+      before { post :create, discipline: valid_attributes }
+      
+      it { assigns(:discipline).should be_a(Discipline) }
+      it { assigns(:discipline).should be_persisted }
+      it { response.should redirect_to(disciplines_path) }
+    end
+    
+    context "with invalid params" do
+      def do_action(param)
+        post(:create, discipline: param)
+      end
+    
+      before do
+        do_action({name: ""})
+      end
+
+      it { should render_template(:new) }
+      it { should respond_with(:success) }
     end
   end
 
-  describe "GET edit" do
-    it "assigns the requested discipline as @discipline" do
-      discipline = Discipline.create! valid_attributes
-      get :edit, {:id => discipline.to_param}, valid_session
-      assigns(:discipline).should eq(discipline)
+  describe "POST 'update'" do
+    let!(:discipline) { FactoryGirl.create(:discipline) }
+    context "with valid params" do
+      before do
+        put 'update', :id => discipline.to_param, :discipline => { :name => "Geografia" }
+      end
+
+      it { assigns(:discipline).should == discipline }
+      it { redirect_to(disciplines_path) }
+      it { flash[:notice].should == "Discipline was successfully updated." }
+      it { discipline.reload.name.should == "Geografia" }
     end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Discipline" do
-        expect {
-          post :create, {:discipline => valid_attributes}, valid_session
-        }.to change(Discipline, :count).by(1)
+    
+    context "with invalid params" do
+      before do
+        put 'update', :id => discipline.to_param, :discipline => { :name => "" }
       end
 
-      it "assigns a newly created discipline as @discipline" do
-        post :create, {:discipline => valid_attributes}, valid_session
-        assigns(:discipline).should be_a(Discipline)
-        assigns(:discipline).should be_persisted
-      end
-
-      it "redirects to the created discipline" do
-        post :create, {:discipline => valid_attributes}, valid_session
-        response.should redirect_to(Discipline.last)
-      end
+      it { assigns(:discipline).should == discipline }
+      it { render_template 'edit' }
     end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved discipline as @discipline" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Discipline.any_instance.stub(:save).and_return(false)
-        post :create, {:discipline => {}}, valid_session
-        assigns(:discipline).should be_a_new(Discipline)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Discipline.any_instance.stub(:save).and_return(false)
-        post :create, {:discipline => {}}, valid_session
-        response.should render_template("new")
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested discipline" do
-        discipline = Discipline.create! valid_attributes
-        # Assuming there are no other disciplines in the database, this
-        # specifies that the Discipline created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Discipline.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => discipline.to_param, :discipline => {'these' => 'params'}}, valid_session
-      end
-
-      it "assigns the requested discipline as @discipline" do
-        discipline = Discipline.create! valid_attributes
-        put :update, {:id => discipline.to_param, :discipline => valid_attributes}, valid_session
-        assigns(:discipline).should eq(discipline)
-      end
-
-      it "redirects to the discipline" do
-        discipline = Discipline.create! valid_attributes
-        put :update, {:id => discipline.to_param, :discipline => valid_attributes}, valid_session
-        response.should redirect_to(discipline)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the discipline as @discipline" do
-        discipline = Discipline.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Discipline.any_instance.stub(:save).and_return(false)
-        put :update, {:id => discipline.to_param, :discipline => {}}, valid_session
-        assigns(:discipline).should eq(discipline)
-      end
-
-      it "re-renders the 'edit' template" do
-        discipline = Discipline.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Discipline.any_instance.stub(:save).and_return(false)
-        put :update, {:id => discipline.to_param, :discipline => {}}, valid_session
-        response.should render_template("edit")
-      end
+        
+    it "should redirect to home" do
+      put 'update', :id => discipline.to_param, :discipline => { :name => "Geografia" }
+      response.should redirect_to(disciplines_path)
     end
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested discipline" do
-      discipline = Discipline.create! valid_attributes
-      expect {
-        delete :destroy, {:id => discipline.to_param}, valid_session
-      }.to change(Discipline, :count).by(-1)
-    end
-
-    it "redirects to the disciplines list" do
-      discipline = Discipline.create! valid_attributes
-      delete :destroy, {:id => discipline.to_param}, valid_session
-      response.should redirect_to(disciplines_url)
-    end
+    let!(:discipline) { FactoryGirl.create(:discipline) }
+    
+    context "should find and redirect" do      
+      before do
+        delete :destroy, id: discipline.to_param
+      end
+      
+      it { assigns(:discipline).should == discipline }
+      it { redirect_to(disciplines_path) }
+    end  
+          
+    context "context" do
+      it "should destroy a Discipline" do
+        expect {
+          delete :destroy, id: discipline.to_param
+        }.to change(Discipline, :count).by(-1)
+      end        
+    end    
   end
-
 end
