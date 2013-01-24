a40fb728b9abb858179b6bb7ced76bba4e0b3c37
Rodrigo Reginato Marques <rreginatom@gmail.com>
2013-01-24 15:16:55 -0200

    Improved other controller tests and validate presence

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
+  validates :name,  presence: true  
   attr_accessible :name
 end
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -6,7 +6,7 @@ describe StudentsController do
   # Student. As you add validations to Student, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    { name: "Rodrigo Reginato Marques"}
   end
 
   # This should return the minimal set of values that should be in the session
@@ -58,100 +58,75 @@ describe StudentsController do
     it { render_template 'edit' }
   end
 
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Student" do
-        expect {
-          post :create, {:student => valid_attributes}, valid_session
-        }.to change(Student, :count).by(1)
-      end
-
-      it "assigns a newly created student as @student" do
-        post :create, {:student => valid_attributes}, valid_session
-        assigns(:student).should be_a(Student)
-        assigns(:student).should be_persisted
-      end
-
-      it "redirects to the created student" do
-        post :create, {:student => valid_attributes}, valid_session
-        response.should redirect_to(Student.last)
-      end
+  describe "POST create" do          
+    context "with valid params" do
+      before { post :create, student: valid_attributes }
+      
+      it { assigns(:student).should be_a(Student) }
+      it { assigns(:student).should be_persisted }
+      it { response.should redirect_to(students_path) }
     end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved student as @student" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Student.any_instance.stub(:save).and_return(false)
-        post :create, {:student => {}}, valid_session
-        assigns(:student).should be_a_new(Student)
+    
+    context "with invalid params" do
+      def do_action(param)
+        post(:create, student: param)
+      end
+    
+      before do
+        do_action({name: ""})
       end
 
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Student.any_instance.stub(:save).and_return(false)
-        post :create, {:student => {}}, valid_session
-        response.should render_template("new")
-      end
+      it { should render_template(:new) }
+      it { should respond_with(:success) }
     end
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested student" do
-        student = Student.create! valid_attributes
-        # Assuming there are no other students in the database, this
-        # specifies that the Student created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Student.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => student.to_param, :student => {'these' => 'params'}}, valid_session
+  describe "POST 'update'" do
+    let!(:student) { FactoryGirl.create(:student) }
+    context "with valid params" do
+      before do
+        put 'update', :id => student.to_param, :student => { :name => "Rodrigo Reginato" }
       end
 
-      it "assigns the requested student as @student" do
-        student = Student.create! valid_attributes
-        put :update, {:id => student.to_param, :student => valid_attributes}, valid_session
-        assigns(:student).should eq(student)
-      end
-
-      it "redirects to the student" do
-        student = Student.create! valid_attributes
-        put :update, {:id => student.to_param, :student => valid_attributes}, valid_session
-        response.should redirect_to(student)
-      end
+      it { assigns(:student).should == student }
+      it { redirect_to(students_path) }
+      it { flash[:notice].should == "Student was successfully updated." }
+      it { student.reload.name.should == "Rodrigo Reginato" }
     end
-
-    describe "with invalid params" do
-      it "assigns the student as @student" do
-        student = Student.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Student.any_instance.stub(:save).and_return(false)
-        put :update, {:id => student.to_param, :student => {}}, valid_session
-        assigns(:student).should eq(student)
+    
+    context "with invalid params" do
+      before do
+        put 'update', :id => student.to_param, :student => { :name => "" }
       end
 
-      it "re-renders the 'edit' template" do
-        student = Student.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Student.any_instance.stub(:save).and_return(false)
-        put :update, {:id => student.to_param, :student => {}}, valid_session
-        response.should render_template("edit")
-      end
+      it { assigns(:student).should == student }
+      it { render_template 'edit' }
+    end
+        
+    it "should redirect to home" do
+      put 'update', :id => student.to_param, :student => { :name => "Rodrigo" }
+      response.should redirect_to(students_path)
     end
   end
-
+  
   describe "DELETE destroy" do
-    it "destroys the requested student" do
-      student = Student.create! valid_attributes
-      expect {
-        delete :destroy, {:id => student.to_param}, valid_session
-      }.to change(Student, :count).by(-1)
-    end
-
-    it "redirects to the students list" do
-      student = Student.create! valid_attributes
-      delete :destroy, {:id => student.to_param}, valid_session
-      response.should redirect_to(students_url)
-    end
+    let!(:student) { FactoryGirl.create(:student) }
+    
+    context "should find and redirect" do      
+      before do
+        delete :destroy, id: student.to_param
+      end
+      
+      it { assigns(:student).should == student }
+      it { redirect_to(students_path) }
+    end  
+          
+    context "context" do
+      it "should destroy a Student" do
+        expect {
+          delete :destroy, id: student.to_param
+        }.to change(Student, :count).by(-1)
+      end        
+    end    
   end
-
 end
