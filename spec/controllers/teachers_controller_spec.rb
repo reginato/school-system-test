5d56033a06c7bf61d1fb5a799b4dbfd928b89aa7
Rodrigo Reginato Marques <rreginatom@gmail.com>
2013-01-24 17:28:31 -0200

    Fixed all controllers and validates

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -7,7 +7,7 @@ describe TeachersController do
   # Teacher. As you add validations to Teacher, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {name: "Professor Pardal"}
   end
 
   # This should return the minimal set of values that should be in the session
@@ -60,100 +60,76 @@ describe TeachersController do
     it { render_template 'edit' }
   end
 
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Teacher" do
-        expect {
-          post :create, {:teacher => valid_attributes}, valid_session
-        }.to change(Teacher, :count).by(1)
-      end
-
-      it "assigns a newly created teacher as @teacher" do
-        post :create, {:teacher => valid_attributes}, valid_session
-        assigns(:teacher).should be_a(Teacher)
-        assigns(:teacher).should be_persisted
-      end
-
-      it "redirects to the created teacher" do
-        post :create, {:teacher => valid_attributes}, valid_session
-        response.should redirect_to(Teacher.last)
-      end
+  describe "POST create" do          
+    context "with valid params" do
+      before { post :create, teacher: valid_attributes }
+      
+      it { assigns(:teacher).should be_a(Teacher) }
+      it { assigns(:teacher).should be_persisted }
+      it { response.should redirect_to(teachers_path) }
     end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved teacher as @teacher" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Teacher.any_instance.stub(:save).and_return(false)
-        post :create, {:teacher => {}}, valid_session
-        assigns(:teacher).should be_a_new(Teacher)
+    
+    context "with invalid params" do
+      def do_action(params)
+        post(:create, teacher: params)
+      end
+    
+      before do
+        do_action({name: ""})
       end
 
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Teacher.any_instance.stub(:save).and_return(false)
-        post :create, {:teacher => {}}, valid_session
-        response.should render_template("new")
-      end
+      it { should render_template(:new) }
+      it { should respond_with(:success) }
     end
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested teacher" do
-        teacher = Teacher.create! valid_attributes
-        # Assuming there are no other teachers in the database, this
-        # specifies that the Teacher created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Teacher.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => teacher.to_param, :teacher => {'these' => 'params'}}, valid_session
+  describe "POST 'update'" do
+    let!(:teacher) { FactoryGirl.create(:teacher) }
+    context "with valid params" do
+      before do
+        put 'update', :id => teacher.to_param, :teacher => valid_attributes
       end
 
-      it "assigns the requested teacher as @teacher" do
-        teacher = Teacher.create! valid_attributes
-        put :update, {:id => teacher.to_param, :teacher => valid_attributes}, valid_session
-        assigns(:teacher).should eq(teacher)
-      end
-
-      it "redirects to the teacher" do
-        teacher = Teacher.create! valid_attributes
-        put :update, {:id => teacher.to_param, :teacher => valid_attributes}, valid_session
-        response.should redirect_to(teacher)
-      end
+      it { assigns(:teacher).should == teacher }
+      it { redirect_to(teachers_path) }
+      it { flash[:notice].should == "Teacher was successfully updated." }
+      it { teacher.reload.name.should == "Professor Pardal" }
     end
-
-    describe "with invalid params" do
-      it "assigns the teacher as @teacher" do
-        teacher = Teacher.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Teacher.any_instance.stub(:save).and_return(false)
-        put :update, {:id => teacher.to_param, :teacher => {}}, valid_session
-        assigns(:teacher).should eq(teacher)
+    
+    context "with invalid params" do
+      before do
+        put 'update', :id => teacher.to_param, :teacher => { :name => "" }
       end
 
-      it "re-renders the 'edit' template" do
-        teacher = Teacher.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Teacher.any_instance.stub(:save).and_return(false)
-        put :update, {:id => teacher.to_param, :teacher => {}}, valid_session
-        response.should render_template("edit")
-      end
+      it { assigns(:teacher).should == teacher }
+      it { render_template 'edit' }
+    end
+        
+    it "should redirect to home" do
+      put 'update', :id => teacher.to_param, :teacher => valid_attributes
+      response.should redirect_to(teachers_path)
     end
   end
-
+  
   describe "DELETE destroy" do
-    it "destroys the requested teacher" do
-      teacher = Teacher.create! valid_attributes
-      expect {
-        delete :destroy, {:id => teacher.to_param}, valid_session
-      }.to change(Teacher, :count).by(-1)
-    end
-
-    it "redirects to the teachers list" do
-      teacher = Teacher.create! valid_attributes
-      delete :destroy, {:id => teacher.to_param}, valid_session
-      response.should redirect_to(teachers_url)
-    end
+    let!(:teacher) { FactoryGirl.create(:teacher) }
+    
+    context "should find and redirect" do      
+      before do
+        delete :destroy, id: teacher.to_param
+      end
+      
+      it { assigns(:teacher).should == teacher }
+      it { redirect_to(teachers_path) }
+    end  
+          
+    context "destroy" do
+      it "should destroy a Teacher" do
+        expect {
+          delete :destroy, id: teacher.to_param
+        }.to change(Teacher, :count).by(-1)
+      end        
+    end    
   end
 
 end
