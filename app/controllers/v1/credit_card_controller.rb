fe727810668fea2d38a94a0517eeafa07336520b
Rodrigo Reginato <rreginatom@gmail.com>
2021-06-09 15:37:23 -0300

    Added new controller

---
+++ app/controllers/v1/credit_card_controller.rb
@@ -0,0 +1,33 @@
+module Api
+    module V1
+      class CreditCardsController < ApiBaseController
+  
+        private
+  
+          def credit_card_params
+            case action_name
+              when 'create'
+                # add , :document ?
+                params.require(:credit_card).permit(:pan, \
+                  :first_name, :last_name, :brand, \
+                  :expiration_month, :expiration_year, :cvc, \
+                  :country, :state, :city, :zip_code, :district, \
+                  :street, :number, :complement, :phone
+                ).merge({
+                    user_id: @current_user.id
+                  })
+              when 'update'
+                params.require(:credit_card).permit( \
+                  :country, :state, :city, :zip_code, :district, \
+                  :street, :number, :complement, :phone
+                )
+            end
+          end
+  
+          def query_params
+            {}
+          end
+  
+      end
+    end
+  end
