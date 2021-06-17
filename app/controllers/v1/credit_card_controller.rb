973b6e66a5ab3a583b9744519e7171af19e96885
Rodrigo Reginato <rreginatom@gmail.com>
2021-06-17 15:58:05 -0300

    Fixed

--- app/controllers/v1/credit_card_controller.rb
+++ app/controllers/v1/credit_card_controller.rb
@@ -12,7 +12,7 @@ module Api
                   :first_name, :last_name, :brand, \
                   :expiration_month, :expiration_year, :cvc, \
                   :country, :state, :city, :zip_code, :district, \
-                  :street, :numbesdr, :complement, :phone, :name
+                  :street, :numbesdr, :complement, :phone
                 ).merge({
                     user_id: @current_user.id
                   })
